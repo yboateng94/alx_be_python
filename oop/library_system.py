ee0f3fb5dff6ff8013f6fa8ac32dba0d5f1d383b
Yeboah Boateng <ybdecipher@outlook.com>
2025-01-24 13:40:20 +0000

    library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -5,6 +5,9 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
